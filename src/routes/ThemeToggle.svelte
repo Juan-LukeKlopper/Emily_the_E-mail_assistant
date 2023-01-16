b6f043825ec5475c1526add839a0a4411fab696a
Juan-LukeKlopper <luke@stakeordie.com>
2023-01-16 18:49:08 +0200

    feat: Make theme toggle button green

--- src/routes/ThemeToggle.svelte
+++ src/routes/ThemeToggle.svelte
@@ -33,7 +33,7 @@
 		data-testid="theme-toggle"
 		on:click={toggleDarkMode}
 		type="button"
-		class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+		class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-green-600 hover:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
 		>{currentTheme}</button
 	>
 </div>
