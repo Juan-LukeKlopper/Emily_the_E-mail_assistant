aaaa04e026ea66e07aa506bfc77885df0f019943
Juan-LukeKlopper <luke@stakeordie.com>
2023-01-16 18:49:33 +0200

    feat: Make page color match theme

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -14,7 +14,7 @@
 </script>
 
 
-<nav class="dark:bg-gray-800 bg-secondary">
+<nav class="dark:bg-black bg-white">
 	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 		<div class="flex h-16 items-center justify-between">
 			<div class="flex items-center w-full">
