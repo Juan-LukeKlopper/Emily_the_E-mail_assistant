fff7feda5604549e5b7b15e3d536fc89d1d9be0a
Juan-LukeKlopper <luke@stakeordie.com>
2023-01-16 17:34:16 +0200

    feat: Add Navbar and theme toggling to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.postcss';
+	import Navbar from './Navbar.svelte';
 </script>
 
+<body lang="en" class="bg-white dark:bg-black w-full h-full ">
+<Navbar />
+
 <slot />
+</body>
