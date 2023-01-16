1fcad3e38f2708813cacb712070056bda1104515
Juan-LukeKlopper <luke@stakeordie.com>
2023-01-16 18:48:17 +0200

    feat: Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>About Emily the E-mail assistant</title>
+	<meta name="E-mail assistant about page" content="About section of Emily, An AI powered E-mail response assistant" />
+</svelte:head>
+
+<h1 class="text-6xl dark:text-white text-gray-900 text-center">About Emily.</h1>
+<p class="text-xl dark:text-white text-gray-900 text-center">
+    This project is a E-mail response generating website which connects to OpenAI's GPT-3 Large Language Model. 
+    It allows users to simply paste the text of the email they wish to respond to in the text box and click the generate button.
+    The website will then send the text to A custom API where it will generate a response which is then displayed on the page.
+    The user can then copy the response and paste it into their email client or simply click the send button to send the email.(This feature is not yet implemented)
+</p>
