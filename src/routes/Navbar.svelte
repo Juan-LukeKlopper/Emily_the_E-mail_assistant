1f88769112b2ed1d65aa880d5ec22e76e31ff9e7
Juan-LukeKlopper <luke@stakeordie.com>
2023-01-18 00:44:11 +0200

    feat: Update logo and add margin to active link buttons

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -11,35 +11,29 @@
 			title: 'About'
 		}
 	];
-	import light_logo from '../libs/assets/light_logo.png';
-	import dark_logo from '../libs/assets/dark_logo.png';
+	import logo from '../libs/assets/logo.png';
 </script>
 
 
-<nav class="dark:bg-black bg-white">
-	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+<nav class="dark:bg-black bg-white mb-20 top-0">
+	<div class="w-full">
 		<div class="flex h-16 items-center justify-between">
 			<div class="flex items-center w-full">
-				<div class="flex-shrink-0">
+				<div class="flex-auto">
 					<img
-						class="h-8 w-8 dark:hidden"
-						src={light_logo}
-						alt="Emily the E-mail assistant"
-					/>
-					<img
-						class="h-50 w-50 hidden dark:block overflow-visible m-20 mb-10"
-						src={dark_logo}
+						class="h-14 w-10 mt-20 mr-10 align-baseline object-cover sh"
+						src={logo}
 						alt="Emily the E-mail assistant"
 					/>
 				</div>
 				<div class="hidden md:block w-full">
-					<div class="ml-10 flex items-baseline space-x-4">
+					<div class="mx-15 flex justify-end space-x-4 mt-14 mb-1/3">
 						{#each links as { href, title }}
 							<ActiveLink
 								{href}
-								baseClass="px-3 py-2 rounded-md text-sm font-medium"
-								activeClass="bg-gray-200 text-gray-900 dark:text-gray-300 dark:bg-gray-900 dark:text-white"
-								defaultClass="text-gray-900 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-900"
+								baseClass="px-3 py-2 rounded-md text-sm font-medium mx-15"
+								activeClass="bg-gray-200 text-gray-900 dark:text-gray-300 dark:bg-gray-900 dark:text-white mx-15"
+								defaultClass="text-gray-900 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-900 mx-15"
 							>
 								{title}
 							</ActiveLink>
