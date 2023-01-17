8362d1dddf7ce465b8c0f2e2cd5e1afcb5405998
Juan-LukeKlopper <luke@stakeordie.com>
2023-01-18 00:46:58 +0200

    style: Add margin to h1 & p + update border & bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,12 @@
 	<meta name="E-mail assistant landing page" content="Emily, An AI powered E-mail response assistant" />
 </svelte:head>
 
-<h1 class="text-6xl dark:text-white text-gray-900 text-center">Welcome to Emily</h1>
-<p class="text-xl dark:text-white text-gray-900 text-center">The AI powered E-mail response assistant</p>
+<h1 class="text-6xl dark:text-white text-gray-900 text-center my-5">Welcome to Emily</h1>
+<p class="text-xl dark:text-white text-gray-900 text-center my-5">The AI powered E-mail response assistant</p>
 
 <div class="flex ">
 	<div class="bg-white dark:bg-black w-1/5"/>
-	<textarea cols="30" rows="10" bind:value={text} class="w-3/5 h-6/12 text-center" />
+	<textarea cols="30" rows="10" bind:value={text} class="w-3/5 h-6/12 text-center border-double border-2 border-black dark:border-white bg-mediumlightGray" />
 	<div class="bg-white dark:bg-black w-1/5 "/>
 </div>
 
