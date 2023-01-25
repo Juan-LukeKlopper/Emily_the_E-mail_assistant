fb7e033c81bcf53674879795108fe95e333fdbc6
Juan-LukeKlopper <luke@stakeordie.com>
2023-01-25 15:37:03 +0200

    feat: Add serverside code to call openAi's API

---
+++ src/routes/api/generate/+server.ts
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from 'openai';
+import type { RequestHandler } from '../$types';
+import { env } from '$env/dynamic/private';
+
+
+const configuration = new Configuration({
+  apiKey: env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "Please respond to the following email in the tone of Lex Fridman without saying his name.      Email:";
+const basePromtSuffix = " Response: "
+
+export const POST: RequestHandler = async (event) => {
+    const request = await event.request.json();
+    const userInput: string = request['text'];
+
+    
+    // Run first prompt
+    console.log(`API: ${basePromptPrefix}${userInput}${basePromtSuffix}`)
+
+    
+    const baseCompletion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${basePromptPrefix}${userInput}${basePromtSuffix}`,
+        temperature: 0.7,
+        max_tokens: 250,
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+
+    return new Response(JSON.stringify({ output: basePromptOutput}), {
+        headers: {
+            'content-type': 'application/json',
+        },
+    });
+};
